e47647168b7c0f5d563631950ec6d43059f518e7
Fotios Pechlivanis <fotispechli@gmail.com>
2023-05-31 17:10:16 +0300

    Implement basic haiku generator functionality

--- haiku/generator.py
+++ haiku/generator.py
@@ -1,28 +1,19 @@
 import random
 import nltk
+import re
 from nltk.corpus import cmudict
 
-# Download the CMU Pronouncing Dictionary if not already downloaded
 nltk.download('cmudict')
-
-# Load the CMU Pronouncing Dictionary
 pronouncing_dict = cmudict.dict()
 
 
 def count_syllables(word):
-    # Function to count the number of syllables in a word
     word = word.lower()
-
-    # Check if word is present in the CMU Pronouncing Dictionary
     if word in pronouncing_dict:
-        # Each entry in the dictionary represents a list of phonemes for the word
-        # Count the number of phonemes that end with a digit, indicating stress
         syllable_count = len(
             [ph for ph in pronouncing_dict[word][0] if ph[-1].isdigit()])
         return syllable_count
 
-    # If word is not found in the dictionary, use a simple fallback method
-    # Count the number of vowel sounds (approximation) in the word
     vowels = 'aeiouy'
     syllable_count = 0
     prev_char_is_vowel = False
@@ -37,52 +28,56 @@ def count_syllables(word):
     return syllable_count
 
 
+def generate_line(words, syllables):
+    line = []
+    while syllables > 0:
+        word = random.choice(words)
+        syllable_count = count_syllables(word)
+
+        if syllable_count <= syllables:
+            line.append(word)
+            syllables -= syllable_count
+
+    return ' '.join(line)
+
+
 def generate_haiku_from_text(text_input):
-    lines = text_input.split('\n')
-    haiku_lines = []
-    for line in lines:
-        words = line.split()
-        syllable_count = sum(count_syllables(word) for word in words)
-        haiku_lines.append(words)
+    words = text_input.split()
+    haiku = []
 
-        if syllable_count == 5:
-            break
+    line = generate_line(words, 5)
+    haiku.append(line)
 
-    if len(haiku_lines) != 3:
-        return None
+    line = generate_line(words, 7)
+    haiku.append(line)
 
-    haiku = '\n'.join(' '.join(line) for line in haiku_lines)
-    return haiku
+    line = generate_line(words, 5)
+    haiku.append(line)
+
+    return '\n'.join(haiku)
+
+
+def clean_code(code):
+    cmu_dict = cmudict.words()
+    words = re.findall(r'\b[a-zA-Z]+\b', code)
+    readable_words = [word for word in words if len(
+        word) > 1 and word.lower() in cmu_dict and word.isalnum()]
+
+    return ' '.join(readable_words)
 
 
 def generate_haiku_from_code(code):
-    lines = code.split('\n')
+    cleaned_code = clean_code(code)
+    haiku = generate_haiku_from_text(cleaned_code)
 
-    # Filter out empty lines and comments
-    lines = [line for line in lines if line.strip(
-    ) and not line.strip().startswith('#')]
-
-    haiku_lines = []
-    syllable_count = 0
-    for line in lines:
-        words = line.split()
-        for word in words:
-            syllable_count += count_syllables(word)
-        haiku_lines.append(words)
-
-        if syllable_count >= 17:
-            break
-
-    if len(haiku_lines) < 3 or syllable_count != 17:
-        return None
-
-    haiku = '\n'.join(' '.join(line) for line in haiku_lines[:3])
     return haiku
 
 
-def generate_haiku(text_input=None, github_repo_url=None):
+def generate_haiku(text_input=None, code_input=None, github_repo_url=None):
     if text_input:
         return generate_haiku_from_text(text_input)
+    elif code_input:
+        return generate_haiku_from_code(code_input)
     elif github_repo_url:
         # Fetch the GitHub repository and code
         # code = fetch_code_from_github_repo(github_repo_url)
